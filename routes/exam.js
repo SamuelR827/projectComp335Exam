84b31f4d6282b68f7a4a1937c036650e833090c8
SamuelR827 <srrantz17@gmail.com>
2024-04-11 22:32:39 -0400

    implement part 1, 2, 3

--- routes/exam.js
+++ routes/exam.js
@@ -91,13 +91,106 @@ router.post('/addAssignment',function(req, res, next) {
             next(err);
           }
           console.log("Assignment creation is successful");
-          res.redirect('/exam/addAssignment');
+          res.redirect("/exam/addAssignment?message=Assignment+creation+is+successful")
         });
     }
     else {
       console.log("user doesn't exist");
-      res.redirect('/exam/addAssignment');
+      res.redirect("/exam/addAssignment?message=User+does+not+exist");
+
     }
+
+  });
+});
+
+router.get('/changePassword',function(req, res, next) {
+  res.sendFile(path.join(__dirname,'..', 'public','changePassword.html'));
+});
+
+router.post('/changePassword', function(req, res, next) {
+    const currentPassword = req.body.currentPassword
+    const newPassword = req.body.newPassword
+    const confirmPassword = req.body.confirmPassword
+    const username = req.user.username
+
+    client.query("SELECT password FROM examusers WHERE username = $1", [username], function(err, result){
+      if (err){
+        console.log("unable to query SELECT");
+        next(err);
+      }
+      const encryptedPassword = result.rows[0].password;
+
+      bcrypt.compare(currentPassword, encryptedPassword, function(err, match){
+        if (err) {
+          console.log("unable to bcrypt")
+          next(err)
+        }
+        else if (!match){
+          console.log("current password does not match")
+          res.redirect("/exam/changePassword?message=current+password+does+not+match");
+        }
+        else if (newPassword !== confirmPassword){
+          console.log("The two passwords you entered are not the same");
+          res.redirect("/exam/changePassword?message=The+two+password+you+entered+are+not+the+same");
+        }
+      else {
+      bcrypt.hash(newPassword, 10, function(err, hashedPassword){
+        if (err){
+          console.log("unable to bcrypt")
+          next(err)
+        }
+      client.query("UPDATE examusers set password = $1 where username=$2", [hashedPassword, username], function(err, result){
+        if (err){
+          console.log("unable to query UPDATE");
+          next(err);
+        }
+        console.log("You successfully changed password");
+        res.redirect("/exam/changePassword?message=You+successfully+changed+password");
+          });
+        });
+      }
+    });
+  });
+});
+
+
+router.get('/addUSer',function(req, res, next) {
+  res.sendFile(path.join(__dirname,'..', 'public','addUSer.html'));
+});
+
+router.post('/addUser', function(req, res, next){
+  const username = req.body.username;
+  const password = req.body.password;
+  const isadmin = req.body.isadmin;
+
+  bcrypt.hash(password, 10, function(err, hashedPassword){
+    if (err){
+      console.log("unable to bcrypt");
+      return next(err);
+    }
+
+    client.query('SELECT * FROM examusers WHERE username = $1', [username], function(err, result) {
+      if (err){
+        console.log("unable to query SELECT")
+        next(err);
+      }
+      if (result.rows.length > 0){
+        console.log("User exits")
+        res.redirect("/exam/addUser?message=User+exists");
+      }
+      else {
+
+    client.query('INSERT INTO examusers (username, password, isadmin) VALUES ($1, $2, $3)', [username, hashedPassword, isadmin], function(err, result) {
+      if (err) {
+        console.log("unable to query INSERT");
+        next(err);
+      }
+
+      console.log("User creation is successful");
+      res.redirect("/exam/addUser?message=User+creation+is+successful");
+    });
+    }
+    });
   });
 });
 
